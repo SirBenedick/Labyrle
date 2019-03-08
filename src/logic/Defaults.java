54e52566d8755023927727e4f2028f8696aafe6d
TehKuh <TehKuh@141.19.157.245>
2019-03-08 09:01:08 +0100

    Tilemap

---
+++ src/logic/Defaults.java
@@ -0,0 +1,7 @@
+package logic;
+
+public class Defaults
+{
+	public static final int LabyrinthWidth = 8;
+	public static final int LabyrinthHeight = 8;
+}
--- src/logic/Tilemap.java
+++ src/logic/Tilemap.java
@@ -2,5 +2,29 @@ package logic;
 
 public class Tilemap
 {
-
+	/**
+	 * 	The actual tilemap. Access order is X/Y.
+	 */
+	private Tile tiles[][];
+	
+	
+	public Tilemap() 
+	{
+		tiles = new Tile[Defaults.LabyrinthWidth][Defaults.LabyrinthHeight];
+		for (int x = 0; x < Defaults.LabyrinthWidth; x++)
+		{
+			for (int y = 0; y < Defaults.LabyrinthHeight; y++)
+				tiles[x][y] = new Tile();
+		}
+	}
+	
+	public int getWidth()
+	{
+		return Defaults.LabyrinthWidth;
+	}
+	
+	public int getHeight()
+	{
+		return Defaults.LabyrinthHeight;
+	}
 }
