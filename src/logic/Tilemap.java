32eb636049b18416c17bb56d922843b4d3e58338
TehKuh <TehKuh@141.19.157.245>
2019-03-08 11:05:45 +0100

    Tilemap & Testing

--- src/logic/Tilemap.java
+++ src/logic/Tilemap.java
@@ -1,5 +1,16 @@
 package logic;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * FILE FORMAT BYTE[2] "TQ" INT mapWidth INT mapHeight for x to width for y to
+ * height INT type INT color INT target_color INT Start count For each start INT
+ * x INT y BYTE[2] "TQ"
+ * 
+ */
 public class Tilemap
 {
 	/**
@@ -72,18 +83,138 @@ public class Tilemap
 		return Defaults.LabyrinthHeight;
 	}
 
+	public void setStartingPoint(Color color, int x, int y)
+	{
+		if (color == Color.NONE)
+			return;
+
+		removeStartingPoint(color);
+
+		tiles[x][y].setColor(color);
+		startingPoints[color.ordinal()] = new TileInformation(tiles[x][y], x, y);
+	}
+
+	public void removeStartingPoint(Color color)
+	{
+		if (color == Color.NONE)
+			return;
+
+		if (startingPoints[color.ordinal()] == null)
+			return;
+
+		startingPoints[color.ordinal()].getTile().setColor(Color.NONE);
+		startingPoints[color.ordinal()] = null;
+	}
+
 	public Tile[][] getTiles()
 	{
 		return tiles;
 	}
 
+	public void setEndPoint(int x, int y)
+	{
+		removeEndPoint();
+		endPoint = new TileInformation(tiles[x][y], x, y);
+	}
+
+	public void removeEndPoint()
+	{
+		endPoint = null;
+	}
+
 	public void saveToFile(String path) throws Exception
 	{
+		OutputStream out = new FileOutputStream(path);
 
+		// Write identification string
+		out.write(new byte[] { 'T', 'Q' });
+
+		// Write map Boundaries
+		out.write(getWidth());
+		out.write(getHeight());
+
+		// Write tile data
+		for (int x = 0; x < getWidth(); x++)
+		{
+			for (int y = 0; y < getHeight(); y++)
+			{
+				out.write(tiles[x][y].getType().ordinal());
+				out.write(tiles[x][y].getColor().ordinal());
+				out.write(tiles[x][y].getTarget().ordinal());
+			}
+		}
+
+		// Write starting points
+		out.write(getStartingPointCount());
+		for (int i = 0; i < startingPoints.length; i++)
+		{
+			if (startingPoints[i] == null)
+				continue;
+
+			out.write(startingPoints[i].getX());
+			out.write(startingPoints[i].getY());
+		}
+
+		// Write map validation string
+		out.write(new byte[] { 'T', 'Q' });
+
+		out.close();
 	}
 
 	public void loadFromFile(String path) throws Exception
 	{
+		InputStream in = new FileInputStream(path);
+		byte[] validation_header = new byte[2];
 
+		// Read file id
+		in.read(validation_header);
+		if (validation_header[0] != 'T' || validation_header[1] != 'Q')
+		{
+			in.close();
+			throw new Exception("File is not a labyrinth, header failure");
+		}
+		
+		// Read tile data
+		int width = in.read();
+		int height = in.read();
+		
+		if (width != Defaults.LabyrinthWidth || height != Defaults.LabyrinthHeight)
+		{
+			in.close();
+			throw new Exception("The labyrinth boundaries do not match the defaults.");
+		}
+		
+		
+
+		// Read starting points
+		int startingPointCount = in.read();
+		if (startingPointCount < 0 || startingPointCount > 4)
+		{
+			in.close();
+			throw new Exception("Labyrinth is corrupted. Starting point count is not between 1 and 4");
+		}
+
+		for (int i = 0; i < startingPointCount; i++)
+			setStartingPoint(Color.values()[i], in.read(), in.read());
+
+		// Validate to be sure the correct amount of bytes have been read
+		in.read(validation_header);
+		if (validation_header[0] != 'T' || validation_header[1] != 'Q')
+		{
+			in.close();
+			throw new Exception("File is not a labyrinth, validation failure");
+		}
+
+		in.close();
+	}
+
+	public static Tilemap makeTestMap()
+	{
+		Tilemap map = new Tilemap();
+		map.setStartingPoint(Color.COLOR0, 0, 0);
+		map.setStartingPoint(Color.COLOR1, 0, map.getHeight() - 1);
+		map.setEndPoint(map.getWidth() - 1, map.getHeight() - 1);
+
+		return map;
 	}
 }
