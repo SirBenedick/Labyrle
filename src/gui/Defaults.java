8b7e6235c758024c56444c6a0521cfad058079dc
Maximilian <student@192.168.101.20>
2019-03-14 17:37:04 +0100

    Farben Endpunkte geändert

--- src/gui/Defaults.java
+++ src/gui/Defaults.java
@@ -7,11 +7,11 @@ public class Defaults
 	public static final int TILE_SIZE = 32;
 	public static Color[] COLOR_TABLE = new Color[]
 	{
-		Color.RED,
-		Color.GREEN,
-		Color.BLUE,
-		Color.YELLOW
+		Color.web("#227065"),
+		Color.web("#66615d"),
+		Color.web("#702237"),
+		Color.web("#647022")
 	};
 	public static final Color WALL_COLOR = Color.BLACK;
-	public static final Color SHADOW_COLOR = Color.BLACK;
+	public static final Color SHADOW_COLOR = Color.WHITE;
 }
