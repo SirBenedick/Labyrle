9854f3b29fde63bec467411f5b999deeeebdad04
Kaninala <alena.rosler@hotmail.com>
2019-03-18 11:33:26 +0100

    Farben wurden angepasst

--- src/gui/Defaults.java
+++ src/gui/Defaults.java
@@ -7,10 +7,10 @@ public class Defaults
 	public static final int TILE_SIZE = 32;
 	public static Color[] COLOR_TABLE = new Color[]
 	{
-		Color.web("#227065"),
-		Color.web("#66615d"),
-		Color.web("#702237"),
-		Color.web("#647022")
+		Color.web("#00BA6B"),
+		Color.web("#BA4C00"),
+		Color.web("#9708A3"),
+		Color.web("#095ABA")
 	};
 	public static final Color WALL_COLOR = Color.BLACK;
 	public static final Color SHADOW_COLOR = Color.rgb(219, 219, 219);
